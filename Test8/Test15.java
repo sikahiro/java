c4aa2f36ecec7ebb60a9346644338f053e2646c7
sikahiro <ktq2390321@stu.o-hara.ac.jp>
2024-01-29 15:44:55 +0900

    Commit

--- Test8/Test15.java
+++ Test8/Test15.java
@@ -1,20 +1,19 @@
 package Test8;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-class Test14 {
+public class Test15 {
     public static void main(String[] args){
         Map<String,String> map =new LinkedHashMap<>();
-        map.put("野菜", "季節");
+        System.out.println("野菜    :季節");
         map.put("キャベツ", "春");
         map.put("スイカ", "夏");
         map.put("ナス", "秋");
         map.put("ハクサイ", "冬");
-        for (String key:map.keySet()){
-            String value =map.get(key);
-            System.out.println(key+":"+value);
-        }
+        for (String key:map.keySet())
+            if ((map.get(key))=="春")
+                System.out.println(key+":"+map.get(key));
+        
     }
 }
