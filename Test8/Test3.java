6761d613bc2b4dbe9bb76954391338e0d91be730
sikahiro <ktq2390321@stu.o-hara.ac.jp>
2024-01-29 14:19:02 +0900

    Commit

--- Test8/Test3.java
+++ Test8/Test3.java
@@ -3,8 +3,23 @@ import java.util.*;
 class Test3 {
     public static void main(String[] args){
         Scanner stdIn = new Scanner(System.in);
-        for (int i = 1; i < 10;i++)
-            System.out.println(i+"件目:整数を入力=");
+        int[] input =new int[10];
+        List<Integer> even =new ArrayList<>();
+        List<Integer> odd=new ArrayList<>(); 
+        for (int i = 0; i < 10;i++){
+            System.out.print(i+1+"件目:整数を入力=");
+            input[i] = stdIn.nextInt();
+        }
+        for (int num:input)
+            if (num %2==0)
+                even.add(num);
+            else
+                odd.add(num);
+        System.out.println("偶数値配列="+even);
+        System.out.println("奇数値配列="+odd);
+                
+
+
             
 
     }
